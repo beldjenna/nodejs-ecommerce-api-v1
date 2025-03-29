8b50c48147504f3f3039240d991f18a05fc0f571
Bilal <beldjenna.billal@sefrone.com>
2025-03-30 00:57:02 +0100

    connect with db (mongoDB)

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 const express = require("express");
 const app = express();
 
-const dotenv = require("dotenv");           //config.env
+//config.env
+const dotenv = require("dotenv");           
 dotenv.config({path: 'config.env'});
 
+//connect with db
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_URI).then((conn) => {
+    console.log(`Database is connected: ${conn.connection.host}`);
+}).catch((err) => {
+    console.error(`Database Error ${err}`);
+    process.exit(1);
+});
+
+//Middlewares
 app.use(express.json());
+const morgan = require("morgan");           
+if(process.env.NODE_ENV == "development"){
+    app.use(morgan('dev'));
+}
+
+//1-create schema
+const categorySchema = new mongoose.Schema({
+    name: String,
+});
+//2-create model
+const CategoryModel = mongoose.model('Category', categorySchema);
+
+//Routes
+app.post("/", (req, res) => {
+    const name = req.body.name;
+    const category = new CategoryModel({name});
+
+    category.save().then((doc) => {
+        res.json(doc);
+    }).catch((err) => {
+        res.json(err);
+    })
 
 
+});
 
 const PORT = process.env.PORT || 8000;              // default value is 8000
 app.listen(PORT, () => {
