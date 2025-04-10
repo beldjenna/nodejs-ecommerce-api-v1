80e9ba96d042c171daeb15fbee41c2bc24d1de37
Bilal <beldjenna.billal@sefrone.com>
2025-04-10 14:17:09 +0100

    pagination

--- services/categoryService.js
+++ services/categoryService.js
@@ -3,8 +3,11 @@ const asyncHandler = require('express-async-handler')
 const CategoryModel = require("../models/categoryModel");
 
 exports.getCategories = asyncHandler (async(req, res) => {
-    const categories = await CategoryModel.find({});
-    res.status(200).json({results : categories.length, data : categories});
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 5;
+    const skip = (page - 1) * limit;
+    const categories = await CategoryModel.find({}).skip(skip).limit(limit);
+    res.status(200).json({results : categories.length, page, data : categories});
 });
 
 exports.createCategory = asyncHandler(async (req, res) => {
