bce161a00c70d9867d081d3de937239ec0fe4f3e
Bilal <beldjenna.billal@sefrone.com>
2025-04-10 15:45:44 +0100

    patch, delete endpoints

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {getCategories, createCategory, getCategory} = require("../services/categoryService");
+const {getCategories, createCategory, getCategory, updateCategory, deleteCategory} = require("../services/categoryService");
 const router = express.Router();
 
 router.route("/").get(getCategories).post(createCategory);
 router.route("/:id").get(getCategory);
+router.route("/:id").patch(updateCategory).delete(deleteCategory);
 
 module.exports = router;
--- services/categoryService.js
+++ services/categoryService.js
@@ -25,3 +25,22 @@ exports.getCategory = asyncHandler ( async (req, res) => {
     }
     res.status(200).json({data : category});
 });
+
+exports.updateCategory = asyncHandler (async (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+    const category = await CategoryModel.findOneAndUpdate({_id: id}, {name, slug: slugify(name)}, {new : true});
+    if(!category){
+        res.status(404).json({msg : `No category with this id : ${id}`});
+    }
+    res.status(200).json({data : category});
+});
+
+exports.deleteCategory = asyncHandler (async (req, res) => {
+    const {id} = req.params;
+    const category = await CategoryModel.findOneAndDelete(id);
+    if(!category){
+        res.status(404).json({msg : `No category with this id : ${id}`});
+    }
+    res.status(204).json({data : category});
+});
